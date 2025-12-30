2e61dfee007ca9485b01ca5d74d6f00e08448389
Адмін <@DESKTOP-RQHPOUK>
2025-12-30 02:39:05 +0200

    Add project files.

---
+++ lab7/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnAddOne = new System.Windows.Forms.Button();
+            this.btnAddMany = new System.Windows.Forms.Button();
+            this.lblStats = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnAddOne
+            // 
+            this.btnAddOne.Location = new System.Drawing.Point(12, 12);
+            this.btnAddOne.Name = "btnAddOne";
+            this.btnAddOne.Size = new System.Drawing.Size(92, 35);
+            this.btnAddOne.TabIndex = 0;
+            this.btnAddOne.Text = "+1 Car";
+            this.btnAddOne.UseVisualStyleBackColor = true;
+            this.btnAddOne.Click += new System.EventHandler(this.btnAddOne_Click);
+            // 
+            // btnAddMany
+            // 
+            this.btnAddMany.Location = new System.Drawing.Point(12, 53);
+            this.btnAddMany.Name = "btnAddMany";
+            this.btnAddMany.Size = new System.Drawing.Size(92, 33);
+            this.btnAddMany.TabIndex = 1;
+            this.btnAddMany.Text = "+100 Cars";
+            this.btnAddMany.UseVisualStyleBackColor = true;
+            this.btnAddMany.Click += new System.EventHandler(this.btnAddMany_Click);
+            // 
+            // lblStats
+            // 
+            this.lblStats.AutoSize = true;
+            this.lblStats.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblStats.ForeColor = System.Drawing.Color.White;
+            this.lblStats.Location = new System.Drawing.Point(12, 89);
+            this.lblStats.Name = "lblStats";
+            this.lblStats.Size = new System.Drawing.Size(92, 16);
+            this.lblStats.TabIndex = 2;
+            this.lblStats.Text = "Stats: 0 cars";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1222, 539);
+            this.Controls.Add(this.lblStats);
+            this.Controls.Add(this.btnAddMany);
+            this.Controls.Add(this.btnAddOne);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnAddOne;
+        private System.Windows.Forms.Button btnAddMany;
+        private System.Windows.Forms.Label lblStats;
+    }
+}
+
---
+++ lab7/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab7
+{
+    public class CarModel
+    {
+        private Brush _brush;
+        private Color _color;
+
+        public CarModel(Color color)
+        {
+            _color = color;
+            _brush = new SolidBrush(color);
+        }
+
+        public void Draw(Graphics g, int x, int y)
+        {
+            g.FillRectangle(_brush, x, y, 40, 20);
+            g.DrawRectangle(Pens.Black, x, y, 40, 20);
+            g.FillRectangle(Brushes.Cyan, x + 25, y + 2, 10, 16);
+            g.FillEllipse(Brushes.Black, x + 5, y + 15, 10, 10);
+            g.FillEllipse(Brushes.Black, x + 25, y + 15, 10, 10);
+        }
+    }
+
+    public static class CarFactory
+    {
+        private static Dictionary<Color, CarModel> _cache = new Dictionary<Color, CarModel>();
+
+        public static CarModel GetModel(Color color)
+        {
+            if (!_cache.ContainsKey(color))
+            {
+                _cache[color] = new CarModel(color);
+            }
+            return _cache[color];
+        }
+
+        public static int GetCacheSize()
+        {
+            return _cache.Count;
+        }
+    }
+
+    public class Car
+    {
+        private CarModel _model;
+        private int _x, _y;
+        private int _speed;
+
+        public Car(CarModel model, int x, int y, int speed)
+        {
+            _model = model;
+            _x = x;
+            _y = y;
+            _speed = speed;
+        }
+
+        public void Move(int limitX)
+        {
+            _x += _speed;
+            if (_x > limitX) _x = -50;
+        }
+
+        public void Draw(Graphics g)
+        {
+            _model.Draw(g, _x, _y);
+        }
+    }
+    public partial class Form1 : Form
+    {
+        private List<Car> _cars = new List<Car>();
+        private Random _rnd = new Random();
+        private Color[] _colors = { Color.Red, Color.Blue, Color.Green, Color.Yellow, Color.Orange };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void AddRandomCars(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Color color = _colors[_rnd.Next(_colors.Length)];
+                CarModel model = CarFactory.GetModel(color);
+
+                int y = _rnd.Next(50, this.ClientSize.Height - 50);
+                int speed = _rnd.Next(5, 15);
+
+                _cars.Add(new Car(model, -50, y, speed));
+            }
+            UpdateStats();
+        }
+
+        private void UpdateStats()
+        {
+            lblStats.Text = $"Cars on road: {_cars.Count}\n" +
+                            $"Unique Models in Memory: {CarFactory.GetCacheSize()}";
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (var car in _cars)
+            {
+                car.Move(this.ClientSize.Width);
+            }
+            this.Invalidate();
+        }
+
+        private void btnAddOne_Click(object sender, EventArgs e)
+        {
+            AddRandomCars(1);
+        }
+
+        private void btnAddMany_Click(object sender, EventArgs e)
+        {
+            AddRandomCars(100);
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+
+            for (int y = 50; y < this.Height; y += 40)
+            {
+                g.DrawLine(Pens.White, 0, y, this.Width, y);
+            }
+
+            foreach (var car in _cars)
+            {
+                car.Draw(g);
+            }
+        }
+    }
+}
